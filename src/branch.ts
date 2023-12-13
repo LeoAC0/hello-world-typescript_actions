d918285e16be1cd1c5ed6091df59aca5f92e9f11
Leandro Acosta <leo.manuel.acosta@gmail.com>
2023-12-13 13:57:47 -0300

    Crear rama usando hash

--- src/branch.ts
+++ src/branch.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import * as crypto from 'crypto';
 
 import { getClient } from './api';
 
@@ -9,6 +10,13 @@ interface BranchOptions {
     repoName?: string;
 }
 
+// Función para generar un hash SHA-256 único
+const generateUniqueHash = (input: string): string => {
+    const hash = crypto.createHash('sha256');
+    hash.update(input);
+    return hash.digest('hex');
+};
+
 const createBranch = async (options: BranchOptions): Promise<void> => {
     const { branchName, repoOwner, repoName } = options;
     const owner = repoOwner || github.context.repo.owner;
@@ -17,7 +25,7 @@ const createBranch = async (options: BranchOptions): Promise<void> => {
     const headBranchName = 'main';
 
     try {
-        // Crear rama a partir de 'headBranchName'
+        // Usa como referencia la rama 'headBranchName'
         const headBranch = await getClient().repos.getBranch({
             owner,
             repo,
@@ -25,12 +33,15 @@ const createBranch = async (options: BranchOptions): Promise<void> => {
         });
 
         const sha = headBranch.data.commit.sha;
+        
+        // Generar un hash único basado en el nombre de la rama y un timestamp
+        const timestamp = new Date().getTime();
+        const inputForHash = `${options.branchName}-${timestamp}`;
+        const uniqueHash = generateUniqueHash(inputForHash);
 
         // Construir la referencia de la nueva rama
-        const ref = `refs/heads/${options.branchName}`;
-
-        core.info(`Creating branch ${ref} from ${headBranchName} in repo ${owner}/${repo}...`);
-
+        const ref = `refs/heads/${options.branchName}-${uniqueHash}`;
+        
         // Crear la nueva rama utilizando el SHA obtenido de 'headBranchName'
         await getClient().git.createRef({
             owner,
@@ -38,7 +49,8 @@ const createBranch = async (options: BranchOptions): Promise<void> => {
             ref,
             sha,
         });
-
+        
+        core.info(`Creating branch ${ref} from ${headBranchName} in repo ${owner}/${repo}...`);
         core.info(`Branch created successfully: ${ref}`);
     } catch (error) {
         const errorMessage = (error as Error).message;
