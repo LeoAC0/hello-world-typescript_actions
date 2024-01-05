ffcd58c28d2c8e7d8cf2b5bcd0609dccfe54847e
Leandro Acosta <leo.manuel.acosta@gmail.com>
2024-01-05 11:41:13 -0300

    Nueva funcion para buscar PR de backport abiertas

--- src/pr.ts
+++ src/pr.ts
@@ -77,8 +77,49 @@ const createPR = async (head: string, base: string, title: string, body: string,
     throw new Error(`Error creating new PR in repo ${owner}/${repo} from ${head} to ${base}`);
 };
 
+const listOpenBackportPRs = async (repoOwner = undefined, repoName = undefined) => {
+    const owner = repoOwner ? repoOwner : github.context.repo.owner;
+    const repo = repoName ? repoName : github.context.repo.repo;
+    const state = 'open';
+
+    core.info(`Listing open PRs in repo ${owner}/${repo} with "backport" in head and "next" in base...`);
+
+    const parameters = { owner, repo, state };
+    const response = await getClient().pulls.list(parameters);
+
+    const backportPRs = response.data.filter((pr: { head: { ref: string }, base: { ref: string }, number: number }) => {
+        const isBackport = pr.head.ref.toLowerCase().includes('backport');
+        const isNextBase = pr.base.ref === 'next';
+        return isBackport && isNextBase;
+    });
+
+    if (backportPRs.length > 0) {
+        core.info(`Found open PRs with "backport" in head and "next" in base:`);
+        for (const pr of backportPRs) {
+            core.info(`# ${pr.number} (${pr.html_url})`);
+
+            // Actualizar la rama de la PR
+            await updateBranch(pr, 'main');
+        };
+    } else {
+        core.info(`No open PRs found with "backport" in head and "next" in base.`);
+    }
+
+    return backportPRs;
+};
+
+const updateBranch = async (pr: { head: { ref: string }; number: number }, newBase: string) => {
+    await getClient().pulls.updateBranch({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        pull_number: pr.number,
+        base: newBase
+    });
+};
+
 export {
     getOpenPR,
     updatePR,
-    createPR
+    createPR,
+    listOpenBackportPRs
 };
